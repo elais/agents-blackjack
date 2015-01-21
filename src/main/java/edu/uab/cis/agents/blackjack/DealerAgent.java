8ead1ea015a2fb4a0aa9743b84437a0202b08a7d
Elais Jackson <machinebrainx@gmail.com>
2015-01-20 19:58:33 -0600

    totes complete

--- src/main/java/edu/uab/cis/agents/blackjack/DealerAgent.java
+++ src/main/java/edu/uab/cis/agents/blackjack/DealerAgent.java
@@ -1,9 +1,11 @@
 package edu.uab.cis.agents.blackjack;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 /**
  * A dealer takes the {@link Action#HIT} action until their cards total 17 or
@@ -14,10 +16,13 @@ public class DealerAgent implements Agent {
   @Override
   public Action act(List<Card> cards) {
     int total = 0;
+    int tempSum = 0;
     List<Card> table = cards;
     Map map = new HashMap();
+    List<Integer> values = new ArrayList<Integer>();
     // Add values.
-    map.put("ACE",1);
+
+    map.put("ACE",11);
     map.put("TWO",2);
     map.put("THREE",3);
     map.put("FOUR",4);
@@ -29,18 +34,25 @@ public class DealerAgent implements Agent {
     map.put("TEN",10);
     map.put("JACK",10);
     map.put("QUEEN",10);
-    map.put("KING",10);    
+    map.put("KING",10); 
     
     for(Card card : table){
-        total = total + (Integer)map.get(card.getRank().toString());
+        values.add((Integer)map.get(card.getRank().toString()));
+        total += (Integer)map.get(card.getRank().toString());
     }
-    if( total > 17){
+    while(total > 21){
+        tempSum = 0;
+        values.set(values.indexOf(11), 1);
+        for(int i=0; i<values.size(); i++){
+            tempSum += values.get(i);
+        }
+        total = tempSum;
+    }
+    if(total >= 17){
         return Action.STAND;
     }
     else{
         return Action.HIT;
     }
-
-   
   }
 }
